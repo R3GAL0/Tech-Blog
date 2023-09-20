4263c5e65b0d0dc52d5ebe5c64e70bb1b2f7f02c
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-09-20 17:38:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+// const routes = require('./routes');
+// import sequelize connection
+const sequalize = require('./config/connection')
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+// sync sequelize models to the database, then turn on the server
+sequalize.sync({force: false}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+
+})
+
